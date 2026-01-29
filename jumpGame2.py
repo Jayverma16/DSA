22e71d0e52b5da396d3bc8eb9413e1bd6307536c
jay <vermajay208@gmail.com>
2026-01-29 23:30:04 +0530

    code update as test case failed in leetcode

--- jumpGame2.py
+++ jumpGame2.py
@@ -1,16 +1,22 @@
 from typing import List
 def jump(nums: List[int]) -> int:
-    length  = len(nums)
+    n = len(nums)
+    memo = {}
+
     def backtrack(i):
-        memo = {}
-        min_jumps = float("inf")
-        if i>=length-1:
-            return 0 
-        for j in range(1,nums[i]+1):
+        if i >= n - 1:
+            return 0
+
+        if i in memo:
+            return memo[i]
+
+        min_jumps = float('inf')
+        for j in range(1, nums[i] + 1):
             min_jumps = min(min_jumps, 1 + backtrack(i + j))
+
         memo[i] = min_jumps
         return min_jumps
-    
+
     return backtrack(0)
 
 if __name__ == "__main__":
