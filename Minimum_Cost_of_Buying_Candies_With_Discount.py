51034d1a02060a2783fd1e7551660d6854c7727d
jay <vermajay208@gmail.com>
2026-06-01 22:29:45 +0530

    streak v2

--- Minimum_Cost_of_Buying_Candies_With_Discount.py
+++ Minimum_Cost_of_Buying_Candies_With_Discount.py
@@ -1,35 +1,18 @@
 # 2144. Minimum Cost of Buying Candies With Discount
 class Solution:
     def minimumCost(self, cost: list[int]) -> int:
-        
-        ans_cost = 0
-        if len(cost)==2:
-            ans_cost += cost[0] +cost[1] 
-            return ans_cost
-
-        elif len(cost) == 1:
-            ans_cost += cost[0]
-            return ans_cost
-
-        while len(cost)>=2:
-            for _ in range(2):
-                print(cost)
-                max_1 = max(cost)
-                index_v = cost.index(max_1)
-                ans_cost += max_1
-                cost.pop(index_v) 
-            if len(cost)<1:
-                continue   
-            max_ans = max(cost)
-            index_v = cost.index(max_ans)
-            cost.pop(index_v)
-        
-        if len(cost)==1:
-            max_ans = max(cost)
-            index_v = cost.index(max_ans)
-            ans_cost +=max_ans
-            cost.pop(index_v)
-        return ans_cost
+        cost.sort(reverse=True)
+        total_cost = 0
+        length_cost = len(cost)
+        take = 0
+        for i in range(length_cost):
+            if take == 2:
+                take = 0
+                continue
+            total_cost += cost[i] 
+            take+=1
+        return total_cost
+    
 
 if __name__ == "__main__":
     sol = Solution()
