cf946790b983267afa324305fc82c611cb5cdca2
jay <vermajay208@gmail.com>
2026-06-12 22:30:25 +0530

    water ride

--- water_ride.py
+++ water_ride.py
@@ -13,16 +13,18 @@ class Solution:
         for i in range(len(landDuration)):
             land_finish = landDuration[i] + landStartTime[i]
             for j in range(len(waterDuration)):
-                if waterStartTime[j] >= land_finish:
-                    print(min(min_s_land_time,waterDuration[j]+ waterStartTime[j]))
-                    min_s_land_time = min(min_s_land_time,waterDuration[j]+ waterStartTime[j])
+                # if waterStartTime[j] >= land_finish:
+                water_start = max(land_finish, waterStartTime[j])
+                finish_time = water_start + waterDuration[j]
+                min_s_land_time = min(min_s_land_time,finish_time)
         # take water first 
         min_s_water_time = float('inf') 
         for i in range(len(waterDuration)):
             water_finish = waterStartTime[i] + waterDuration[i]
             for j in range(len(landDuration)):
-                if landStartTime[j] >= water_finish:
-                    min_s_water_time = min(min_s_water_time,landDuration[j]+ landStartTime[j])
+                land_start = max(water_finish, landStartTime[j])
+                finish_time = land_start + landDuration[j]
+                min_s_water_time = min(min_s_water_time,finish_time)
         return min(min_s_water_time,min_s_land_time)
 
 
